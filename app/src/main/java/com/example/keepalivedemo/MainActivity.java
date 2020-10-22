ae401bf05ead22389ab6042094445ee7a00bd0d3
NineTailedFox <1037438704@qq.com>
2020-10-22 11:48:08 +0800

    保活第二方案完成

--- app/src/main/java/com/example/keepalivedemo/MainActivity.java
+++ app/src/main/java/com/example/keepalivedemo/MainActivity.java
@@ -2,29 +2,16 @@ package com.example.keepalivedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.keepalivedemo.aty.KeepManager;
-
 public class MainActivity extends AppCompatActivity {
-//    KeepManager keepManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        //1像素保活
-//        keepManager = new KeepManager();
-//        keepManager.registerKeep(this);
-
-//        JobScheduler
-        //        MyJobService.startJob(this);
-
+        startService(new Intent(this,ForegroundService.class));
     }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-//        keepManager.unregisterKeep(this);//Activity退出时解注册
-    }
+
 }
