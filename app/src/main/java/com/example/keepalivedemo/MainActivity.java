c893bd4c7170f8d9dcfb405fccc6c1712380666b
NineTailedFox <1037438704@qq.com>
2020-10-22 18:28:36 +0800

    JobScheduler拉活

--- app/src/main/java/com/example/keepalivedemo/MainActivity.java
+++ app/src/main/java/com/example/keepalivedemo/MainActivity.java
@@ -7,24 +7,13 @@ import android.os.Bundle;
 import com.example.keepalivedemo.aty.KeepManager;
 
 public class MainActivity extends AppCompatActivity {
-//    KeepManager keepManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        //1像素保活
-//        keepManager = new KeepManager();
-//        keepManager.registerKeep(this);
-
 //        JobScheduler
-        //        MyJobService.startJob(this);
+                MyJobService.startJob(this);
 
     }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-//        keepManager.unregisterKeep(this);//Activity退出时解注册
-    }
+
 }
